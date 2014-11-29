00eda28dfdcd5d9004f9779743d2d2c6ad83cdca
SuchDragoon <vemund.vigen@gmail.com>
2014-11-29 16:45:54 +0100

    Main Menu
    Lagt inn meny med tilhørende fil og skriftfont

---
+++ Assets/MenuObject.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuObject : MonoBehaviour {
+
+
+	public bool isQuit = false;
+
+	void OnMouseEnter () {
+
+		renderer.material.color = Color.red;
+
+	}
+
+	void OnMouseExit () {
+
+		renderer.material.color = Color.white;
+
+	}
+
+	void OnMouseDown () {
+
+		if (isQuit) {
+
+				Application.Quit ();
+
+			} 
+
+		else {
+
+			Application.LoadLevel(1);
+
+		}
+
+	}
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
