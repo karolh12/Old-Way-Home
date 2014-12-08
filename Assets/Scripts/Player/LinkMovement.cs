08c419cb51dc08402a69d600889482221e846d2c
Sindre Johansen <mrsnowfire@hotmail.com>
2014-12-08 20:09:40 +0100

    ny push

--- Assets/Scripts/Player/LinkMovement.cs
+++ Assets/Scripts/Player/LinkMovement.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class LinkMovement : Entity {
 
+	Animator anim;
 	// Use this for initialization
 	void Start () {
-	
+		anim = GetComponent<Animator> ();
 	}
 
 	// Update is called once per frame
